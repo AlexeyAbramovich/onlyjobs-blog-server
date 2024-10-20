b8a23de248d54a469ed50e77ed1eab093ca35444
AlexAbra <alexgithub444356@gmail.com>
2024-10-20 16:15:10 +0300

    prisma connected to database, added helmet for basic security, added types for some func params

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { config } from 'dotenv'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import helmet from 'helmet'
 import { postRouter } from './post/post.routes'
+import { prisma } from './prisma'
 import { userRouter } from './user/user.routes'
 
 config()
@@ -8,18 +10,34 @@ config()
 const app = express()
 
 async function main() {
+	const port = process.env.PORT || 4200
+
+	app.use(helmet())
 	app.use(express.json())
 
 	app.use('/posts', postRouter)
 	app.use('/user', userRouter)
 
-	app.use('*', (_, res) => {
-		res.status(404).json({ message: 'Not Found' })
+	app.use('*', (req: Request, res: Response) => {
+		res.status(404).json({ error: `Not Found - ${req.originalUrl}` })
 	})
 
-	app.listen(process.env.PORT, () => {
-		console.log('Server is running on port 4200')
+	app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+		console.log(error.stack)
+		res.status(500).json({ error: 'Something went wrong...' })
+	})
+
+	app.listen(port, () => {
+		console.log(`Server is running on port ${port}`)
 	})
 }
 
 main()
+	.then(async () => {
+		await prisma.$connect()
+	})
+	.catch(async e => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
